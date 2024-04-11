f1d5e56329183252a24b3f44c262f87817969f65
David Mellors <davidroymellors@gmail.com>
2024-04-11 17:18:54 +0200

    Add project files.

---
+++ PROG_ICE_3/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PROG_ICE_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.receiveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(318, 301);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(185, 20);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(318, 134);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(185, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sans Serif Collection", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(311, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Type Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sans Serif Collection", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(311, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Receive Message";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(371, 178);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 4;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // receiveBtn
+            // 
+            this.receiveBtn.Location = new System.Drawing.Point(371, 346);
+            this.receiveBtn.Name = "receiveBtn";
+            this.receiveBtn.Size = new System.Drawing.Size(75, 23);
+            this.receiveBtn.TabIndex = 5;
+            this.receiveBtn.Text = "Receive";
+            this.receiveBtn.UseVisualStyleBackColor = true;
+            this.receiveBtn.Click += new System.EventHandler(this.receiveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.receiveBtn);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Button receiveBtn;
+    }
+}
+
---
+++ PROG_ICE_3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROG_ICE_3
+{
+    public partial class Form1 : Form
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //-------------------------------------------------------------------------------------------------------------
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            MessageQueue mq;
+            if (MessageQueue.Exists(@".\Private$\MyQueue"))
+            {
+                mq = new MessageQueue(@".\Private$\MyQueue");
+            }
+            else
+            {
+                mq = MessageQueue.Create(@".\Private$\MyQueue");
+            }
+
+            // Create a message with an emoji
+            string messageWithEmoji = txtInput.Text + " 😄"; // Add an emoji to the message
+            mq.Send(messageWithEmoji);
+
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------------
+        private void receiveBtn_Click(object sender, EventArgs e)
+        {
+            MessageQueue mq = new MessageQueue(@".\Private$\MyQueue");
+            mq.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+
+            try
+            {
+                System.Messaging.Message msg = mq.Receive(new TimeSpan(0, 0, 3));
+                string messageWithEmoji = msg.Body.ToString();
+                txtOutput.Text = messageWithEmoji;
+            }
+            catch (MessageQueueException ex)
+            {
+                if (ex.MessageQueueErrorCode == MessageQueueErrorCode.IOTimeout)
+                {
+                    txtOutput.Text = "No new messages.";
+                }
+                else
+                {
+                    // Handle other message queue exceptions
+                    txtOutput.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------------------
+
+    }
+}
+//-----------------------------------------------------END-OF-FILE-----------------------------------------------------
